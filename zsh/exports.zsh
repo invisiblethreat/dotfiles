828a7dfc79b3f883b265774823179bb11f05ef26
Scott Walsh <swalsh@securityscorecard.io>
2016-08-25 08:42:08 -0400

    Stupid nano defaults these days...

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -5,3 +5,5 @@ export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
 export LESS_TERMCAP_so=$'\E[38;5;016m\E[48;5;220m'    # begin standout-mode - info box
 export LESS_TERMCAP_ue=$'\E[0m'           # end underline
 export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
+
+export VISUAL=/usr/bin/vim
