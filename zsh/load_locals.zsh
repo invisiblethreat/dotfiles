5d5de010dea5b5bed7ab85926ea952976d27d3f2
Scott Walsh <scott@invisiblethreat.ca>
2019-01-10 09:10:59 -0400

    Manage fonts independently of Vim config

--- zsh/load_locals.zsh
+++ zsh/load_locals.zsh
@@ -1,6 +1,3 @@
 [[ -r ~/.local_profile.zsh ]] && . ~/.local_profile.zsh
 [[ -r ~/.local_exports.zsh ]] && . ~/.local_exports.zsh
-
-# Order matters- If an export is used in an alias, it's helpful if we
-# set it before attempting to use it
 [[ -r ~/.local_aliases.zsh ]] && . ~/.local_aliases.zsh
