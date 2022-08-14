1a4bef1faa3144ccbe7066cb73fcc9953ef08425
Scott Walsh <sw@binaryedge.io>
2022-08-14 13:32:43 -0300

    fixed up listchars. 'tab' is super annoying

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,33 +1,36 @@
 -- :help options
-local options = {
-  cmdheight = 1,
-  colorcolumn = "81,120",
-  cursorline = true,                       -- highlight the current line
-  enc = "utf8",
-  expandtab = true,
-  foldlevelstart = 20,
-  foldmethod = "indent",
-  hidden = true,
-  history = 1000,
-  hlsearch = true,
-  incsearch = true,
-  list = true,
-  number = true,
-  relativenumber = true,
-  scrolloff = 20,
-  shiftwidth = 2,
-  showmatch = true,
-  splitbelow = true,                       -- force splits below current window
-  splitright = true,                       -- force splits right of the current window
-  statusline = "%F%m%r%h%w [Position=%04l,%04v][%p%%] [Lines=%L]",
-  tabstop = 2,
-  termguicolors = true,
-  title = true,
-  undofile = true,
-  undolevels = 1000,
-}
+local o = vim.opt
 
-for k, v in pairs(options) do
-  vim.opt[k] = v
-end
+o.cmdheight = 1
+o.colorcolumn = "81,120"
+o.cursorline = true                       -- highlight the current line
+o.enc = "utf8"
+--o.scriptencoding = "utf8"
+o.expandtab = true
+o.foldlevelstart = 20
+o.foldmethod = "indent"
+o.hidden = true
+o.history = 1000
+o.hlsearch = true
+o.incsearch = true
+o.list = true
+o.listchars = {
+  trail = "",
+  tab = " ",           -- tab requires at least two characters. :help listchars, as the error message is useless
+  precedes = "",
+  extends = "",
+}
+o.number = true
+o.relativenumber = true
+o.scrolloff = 20
+o.shiftwidth = 2
+o.showmatch = true
+o.splitbelow = true                       -- force splits below current window
+o.splitright = true                       -- force splits right of the current window
+o.statusline = "%F%m%r%h%w [Position=%04l,%04v][%p%%] [Lines=%L]"
+o.tabstop = 2
+o.termguicolors = true
+o.title = true
+o.undofile = true
+o.undolevels = 1000
 
