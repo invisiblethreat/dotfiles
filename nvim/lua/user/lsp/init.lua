535c991115ad9c51ecf2a455b6547e4b45d8353a
Scott Walsh <scott@invisiblethreat.ca>
2023-06-03 09:24:56 -0300

    cleanup and fixing on-attach

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -2,28 +2,11 @@ local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then return end
 
 require("mason").setup {ui = {icons = {package_installed = "✓"}}}
-
 require("mason-lspconfig").setup {
     ensure_installed = {"lua_ls", "ruff_lsp", "pyright"}
-    
-
-
-
-
-
 }
--- ✓ jq
- -- 13                                      ✓ lua-language-server lua_ls
- -- 14 async def worker(wid, queue):        ✓ luacheck
- -- 15     while True:                      ✓ luaformatter
- -- 16         target = await queue.get(    ✓ markdownlint
- -- 17         log.info(f"{wid} checking    ✓ pylint
- -- 18         async with httpx.AsyncCli    ✓ pyright
- -- 19             try:                     ✓ ruff
- -- 20                 r = await client.    ✓ ruff-lsp ruff_lsp
 
 require("user.lsp.handlers").setup()
-
 
 local opts = {noremap = true, silent = true}
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
@@ -32,4 +15,3 @@ vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
 
 require("user.lsp.servers-setup")
-
--- nvim/lua/user/lsp/servers-setup.lua
+++ nvim/lua/user/lsp/servers-setup.lua
@@ -46,16 +46,19 @@ require('lspconfig').ruff_lsp.setup {
 
 require('lspconfig').gopls.setup {
     capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol
-                                                                    .make_client_capabilities())
+                                                                    .make_client_capabilities()),
+    on_attach = on_attach
 }
 require('lspconfig').pyright.setup {
     capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol
-                                                                    .make_client_capabilities())
+                                                                    .make_client_capabilities()),
+    on_attach = on_attach
 }
 
 require('lspconfig').lua_ls.setup {
     capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol
                                                                     .make_client_capabilities()),
+    on_attach = on_attach,
     settings = {
         Lua = {
             diagnostics = {
@@ -70,10 +73,9 @@ local null_ls_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_ok then return end
 
 local sources = {
-    -- python
     null_ls.builtins.formatting.isort, null_ls.builtins.formatting.autopep8,
-    null_ls.builtins.formatting.lua_format, null_ls.builtins.formatting.markdownlint
+    null_ls.builtins.formatting.lua_format,
+    null_ls.builtins.formatting.markdownlint
 }
 
-null_ls.setup({sources = sources})
-
+null_ls.setup({sources = sources, debug = true})
