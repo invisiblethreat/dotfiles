6b6f28fe73ff26e59a68b1613320f39666543a95
Scott Walsh <swalsh@coalitioninc.com>
2023-11-09 17:42:41 -0400

    Adding custom snippets

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,7 +2,7 @@ function urlencode() {
   perl -p -e 's/([^A-Za-z0-9])/sprintf("%%%02X", ord($1))/seg'
 }
 
-function research () {
+function research() {
   mkdir -p $1/{inputs,outputs,go,r,python}
   touch $1/go/research.go
   touch $1/r/research.r
@@ -11,11 +11,31 @@ function research () {
   printf "# $1\n\n" > $1/README.md
 }
 
-function xray () {
+function xray() {
   dest=$(curl -skiI $1 | grep Location:|perl -lane 'print $F[1]')
   echo "$1 redirects to $dest"
 }
 
-function title {
+function title() {
     echo -ne "\033]0;"$*"\007"
+}
+
+function make_venv() {
+  python3 -m venv venv-$1
+}
+
+function venv() {
+  if [ ! -z $VIRTUAL_ENV_PROMPT ]; then
+    echo "Deactivating $VIRTUAL_ENV_PROMPT"
+    deactivate
+  else
+    NAME="venv-$(basename $(pwd))"
+    if [ -d $NAME ]; then
+      echo "Entering venv: ($NAME)"
+      source $NAME/bin/activate
+    else
+      make_venv $(basename $(pwd))
+    fi
+  fi
+
 }
