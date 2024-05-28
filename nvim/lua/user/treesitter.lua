6523a4d5cc489e77f798ffae054a9ede4a3c63ab
Scott Walsh <swalsh@coalitioninc.com>
2024-05-28 10:13:41 -0300

    Apply formatting

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -7,17 +7,18 @@ configs.setup {
     },
     auto_install = true,
     sync_install = false,
-    ignore_install = { "" }, -- List of parsers to ignore installing
+    ignore_install = {""}, -- List of parsers to ignore installing
     highlight = {
-        enable = true,     -- false will disable the whole extension
-        additional_vim_regex_highlighting = true,
+        enable = true, -- false will disable the whole extension
+        additional_vim_regex_highlighting = true
 
     },
-    indent = { enable = true, disable = { "yaml" } }
+    indent = {enable = true, disable = {"yaml"}}
 }
 
 -- Define a custom highlight group for Python comments
-vim.api.nvim_set_hl(0, 'CustomPythonComment', { fg = '#fc9526' })
+-- https://neovim.io/doc/user/api.html#nvim_set_hl()
+vim.api.nvim_set_hl(0, 'CustomPythonComment', {fg = '#fc9526', sp= '#ffffff', italic = true})
 
 -- Link the Tree-sitter comment group to the custom group
-vim.api.nvim_set_hl(0, '@comment.python', { link = 'CustomPythonComment' })
+vim.api.nvim_set_hl(0, '@comment.python', {link = 'CustomPythonComment'})
